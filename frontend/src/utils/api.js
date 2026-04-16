33a4575ef39de28952388b620ce4eae6c4fedeb2
Vaishnavi Dhage <vaishnavidhage32@gmail..com>
2026-04-16 16:35:59 +0530

    add deployment config

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: '/api' })
+const baseURL = import.meta.env.VITE_API_URL || '/api'
+
+const api = axios.create({ baseURL: `${baseURL}/api` })
 
 export const uploadFile = (file) => {
   const fd = new FormData()
